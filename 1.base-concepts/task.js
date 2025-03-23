fb546c061213135395a5252c451b315c595ce030
Natasha <nata94192@icloud.com>
2025-03-23 14:11:53 +0700

    задания полностью готовы с правками

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -5,16 +5,17 @@ function solveEquation(a, b, c) {
 	let discriminant = Math.pow(b, 2) - 4 * a * c;
 	if (discriminant < 0) {
 		return arr;
-	} else if (discriminant === 0) {
-		let root = -b / (2 * a);
-		arr.push(root);
-		return arr;
-	} else {
-		let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-		let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-		arr.push(root1, root2);
-		return arr;
 	}
+	let root1, root2;
+	if (discriminant === 0) {
+		root1 = -b / (2 * a);
+		arr.push(root1);
+	} else {
+		root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+		root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+		arr.push(root1, root2);
+	}
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
